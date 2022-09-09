b29d808fba5755a1c1803ac46b05e74eb0bc7a4e
Aleksy <aslepaugo@gmail.com>
2022-09-09 15:38:40 +0200

    run only from main run

--- load_images_into_tg_channel.py
+++ load_images_into_tg_channel.py
@@ -15,23 +15,24 @@ def get_images():
     return images
 
 
-parser = argparse.ArgumentParser(description='Load space photos and images to Telegram Channel')
-parser.add_argument('--hours', help='Delay for the script in hours', type=int)
-parser.add_argument('--photo', help='Photo name from/images to publish', type=str)
-parser.add_argument('--infinite', help='Run infinitely with delay in --hours', type=bool)
-args = parser.parse_args()
-delay_seconds = 4 * 3600 if args.hours is None else args.hours * 3600
-bot = telegram.Bot(token=settings.TG_BOT_TOKEN)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Load space photos and images to Telegram Channel')
+    parser.add_argument('--hours', help='Delay for the script in hours', type=int)
+    parser.add_argument('--photo', help='Photo name from/images to publish', type=str)
+    parser.add_argument('--infinite', help='Run infinitely with delay in --hours', type=bool)
+    args = parser.parse_args()
+    delay_seconds = 4 * 3600 if args.hours is None else args.hours * 3600
+    bot = telegram.Bot(token=settings.TG_BOT_TOKEN)
 
-if args.photo:
-    bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{args.photo}', 'rb'))
-elif not args.infinite:
-    bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{get_images()[0]}', 'rb'))
+    if args.photo:
+        bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{args.photo}', 'rb'))
+    elif not args.infinite:
+        bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{get_images()[0]}', 'rb'))
 
-while True and args.infinite:
-    for image in get_images():
-        image_size = os.path.getsize(f'./images/{image}')
-        if image_size > settings.IMAGE_SIZE_LIMIT:
-            continue
-        bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{image}', 'rb'))
-        time.sleep(delay_seconds)
+    while True and args.infinite:
+        for image in get_images():
+            image_size = os.path.getsize(f'./images/{image}')
+            if image_size > settings.IMAGE_SIZE_LIMIT:
+                continue
+            bot.send_document(chat_id=settings.CHAT_ID, document=open(f'./images/{image}', 'rb'))
+            time.sleep(delay_seconds)
