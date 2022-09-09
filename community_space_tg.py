946dcdf63b8446b552950d232517b363f49a4793
Aleksy <aslepaugo@gmail.com>
2022-09-09 11:27:46 +0200

    publish photo to Telegram channel.

--- community_space_tg.py
+++ community_space_tg.py
@@ -2,4 +2,4 @@ import settings
 import telegram
 
 bot = telegram.Bot(token=settings.TG_BOT_TOKEN)
-bot.send_message(chat_id=settings.CHAT_ID, text="I'm sorry Dave I'm afraid I can't do that.")
+bot.send_document(chat_id=settings.CHAT_ID, document=open('./images/spacex_0.jpg', 'rb'))
